2df769c5559feb88659b0bab899f66d64e1e10a5
ysoroko <ysoroko@student.s19.be>
2021-10-18 15:26:46 +0200

    ex02 finished

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 12:00:03 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/10/18 12:25:17 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/10/18 15:26:21 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,65 @@
 
 int main()
 {
+	// --------------------------------- INTS ---------------------------------
+	std::cout << std::endl << "INTS:" << std::endl;
+	std::cout << "------------------------------------------------" << std::endl << std::endl;
 	MutantStack<int> mstack;
-	mstack.push(5);
+	mstack.push(5); 
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl; // 17 at the top
+	std::cout << "Top element: " << mstack.top() << std::endl; // 17 at the top
 	mstack.pop();
-	std::cout << mstack.size() << std::endl; // 1 because only 5 remains
+	std::cout << "Size: " << mstack.size() << std::endl; // 1 because 17 popped
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
+	mstack.push(-489);
 	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
+	mstack.push(0);
+	mstack.push(192);
+	MutantStack<int>::iterator it = mstack.begin(); // stack is 5 3 5 737 -489 0 0 192
+	std::cout << "Begin: " << *it << std::endl;
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
+	std::cout << std::endl << "All the values in the stack: " << std::endl;
+	std::cout << "--------------------------" << std::endl;
+
 	while (it != ite)
 	{
 		std::cout << *it << std::endl;
 		++it;
 	}
 	std::stack<int> s(mstack);
+	std::cout << std::endl << std::endl << std::endl;
+
+	// ----------------------------------- CHARS ------------------------------
+	std::cout << "CHARS:" << std::endl;
+	std::cout << "------------------------------------------------" << std::endl << std::endl;
+	MutantStack<char>	mcstack;
+	mcstack.push('A'); 
+	mcstack.push('B');
+	std::cout << "Top: " << mcstack.top() << std::endl; // 17 at the top
+	mcstack.pop();
+	std::cout << "Size: " << mcstack.size() << std::endl; // 1 because 17 popped
+	mcstack.push('C');
+	mcstack.push('D');
+	mcstack.push('E');
+	mcstack.push('F');
+	mcstack.push('G');
+	mcstack.push('H');
+	mcstack.push('I');
+	MutantStack<char>::iterator it2 = mcstack.begin(); // stack is A C D E F G H I
+	std::cout << "Begin: " << *it2 << std::endl;
+	MutantStack<char>::iterator it2e = mcstack.end();
+	++it2;
+	--it2;
+	std::cout << std::endl << "All the values in the stack: " << std::endl;
+	std::cout << "--------------------------" << std::endl;
+	while (it2 != it2e)
+	{
+		std::cout << *it2 << std::endl;
+		++it2;
+	}
 	return 0;
 }
--- ex02/mutantstack.hpp
+++ ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: ysoroko <ysoroko@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 11:59:38 by ysoroko           #+#    #+#             */
-/*   Updated: 2021/10/18 12:29:49 by ysoroko          ###   ########.fr       */
+/*   Updated: 2021/10/18 15:14:58 by ysoroko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <algorithm>
 #include <iostream>
 #include <stack>
+#include <vector>
 #include <iterator>
 
 template <typename T>
@@ -24,13 +25,30 @@ class MutantStack : public std::stack<T>
 	public:
 		typedef	T*	iterator;
 		typedef	T const *	const_iterator;
-		//using iterator = T *;
 
-		//using const_iterator = T const *;
-		iterator begin() { return (&this->top()); }
-		iterator end() { return (&(this->top()) + this->size()); }
-		const_iterator begin() const { return this->top(); }
-		const_iterator end() const { return this->top() + this->size(); }
+		iterator begin()
+		{
+			std::vector<T>	v;
+			T				top;
+			iterator		ret;
+			int				n = 0;
+			while (!this->empty())
+			{
+				top = this->top();
+				v.push_back(top);
+				this->pop();
+				n++;
+				
+			}
+			this->push(v[n - 1]);
+			n--;
+			ret = &(this->top());
+			while (n--)
+				this->push(v[n]);
+			return (ret);
+		} //5 3 5 737 0
+
+		iterator end() { return (&this->top() + 1); }
 };
 
 #endif
